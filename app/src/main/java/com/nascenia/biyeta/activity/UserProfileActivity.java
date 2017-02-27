40e4158269988f1231acda2f26d9f538c2bb4d65
ovi_nascenia <sudipta@bdipo.com>
2017-02-27 12:47:48 +0600

    Show full profile of user

--- app/src/main/java/com/nascenia/biyeta/activity/UserProfileActivity.java
+++ app/src/main/java/com/nascenia/biyeta/activity/UserProfileActivity.java
@@ -8,19 +8,23 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
 import com.nascenia.biyeta.R;
+import com.nascenia.biyeta.adapter.PartialProfileViewAdapter;
 import com.nascenia.biyeta.adapter.UserProfileExpenadlbeAdapter;
 import com.nascenia.biyeta.appdata.SharePref;
 import com.nascenia.biyeta.constant.Constant;
+import com.nascenia.biyeta.model.PartialProfileItemModel;
 import com.nascenia.biyeta.model.UserProfile;
 import com.nascenia.biyeta.model.UserProfileChild;
 import com.squareup.okhttp.OkHttpClient;
@@ -42,6 +46,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
 
     private RecyclerView userProfileInfoRecyclerView;
+    TextView aboutMeTextView;
     private ArrayList<UserProfileChild> userProfileChildArrayList;
     private final OkHttpClient client = new OkHttpClient();
     String id;
@@ -50,6 +55,7 @@ public class UserProfileActivity extends AppCompatActivity {
 
     private ArrayList<UserProfileChild> childItemList;
     private ArrayList<UserProfile> userProfilesList;
+    HashMap<String,String> hashMap;
 
 
     @Override
@@ -59,61 +65,30 @@ public class UserProfileActivity extends AppCompatActivity {
         id = getIntent().getExtras().getString("id");
 
         loadUserData();
+        hashMap=new HashMap<>();
+        aboutMeTextView=(TextView)findViewById(R.id.userProfileDescriptionText);
+        setTitle(getIntent().getExtras().getString("user_name"));
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         userProfilesList = new ArrayList<UserProfile>();
 
-      /*  UserProfile ageProfile = new UserProfile("Age", Arrays.asList(new UserProfileChild("age", "23 year"), new UserProfileChild("Height", "4.9"),
-                new UserProfileChild("Height", "4.9"),
-                new UserProfileChild("Height", "4.9"),
-                new UserProfileChild("Height", "4.9"),
-                new UserProfileChild("Height", "4.9"),
-                new UserProfileChild("Height", "4.9"),
-                new UserProfileChild("Height", "4.9")));
-
-        ArrayList<UserProfileChild> list = new ArrayList<UserProfileChild>();
-        list.add(new UserProfileChild("education", "Hsc"));
-        //  UserProfile educationProfile = new UserProfile("Education", Arrays.asList(new UserProfileChild("education", "Hsc")));
-        UserProfile educationProfile = new UserProfile("Education", list);
-
-        UserProfile professionProfile = new UserProfile("Profession", Arrays.asList(new UserProfileChild("profession", "Employee"),
-                new UserProfileChild("profession", "Employee"),
-                new UserProfileChild("profession", "Employee"),
-                new UserProfileChild("profession", "Employee"),
-                new UserProfileChild("profession", "Employee")));
-        List<UserProfile> userProfilesList = Arrays.asList(ageProfile, educationProfile, professionProfile);*/
 
 
         userProfileInfoRecyclerView = (RecyclerView) findViewById(R.id.user_details_recycler_view);
         userProfileInfoRecyclerView.setLayoutManager(new LinearLayoutManager(getBaseContext()));
-        //  userProfileInfoRecyclerView.setAdapter(new UserProfileExpenadlbeAdapter(getBaseContext(), userProfilesList));
-
 
     }
 
-    private void setSampleDataonlist() {
 
-
-        userProfileChildArrayList = new ArrayList<>();
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-        userProfileChildArrayList.add(new UserProfileChild("age", "23 year"));
-
-    }
 
 
     public void loadUserData() {
         new GetData().execute();
     }
 
-
+    ArrayList<PartialProfileItemModel> listPartialProfile;
     class GetData extends AsyncTask<String, String, String> {
         @Override
         protected void onPostExecute(String responseJson) {
@@ -121,36 +96,54 @@ public class UserProfileActivity extends AppCompatActivity {
             //  Toast.makeText(UserProfileActivity.this, responseJson, Toast.LENGTH_SHORT).show();
 
             try {
-                //JSONObject jsonObject = new JSONObject(responseJson);
                 JSONObject parentJsonObj = new JSONObject(responseJson).getJSONObject("profile");
-
-                // JSONObject personalInformationObj = parentJsonObj.getJSONObject("personal_information");
-                //JSONObject professionObj = parentJsonObj.getJSONObject("profession");
-
-
-                Iterator iterator = parentJsonObj.keys();
-                while (iterator.hasNext()) {
-                    String key = (String) iterator.next();
-                    String value = parentJsonObj.getString(key);
-
-                    //  get id from  issue
-                    //String _pubKey = issue.optString("id");
-                    //Log.i("jsonvalue", " key-> " + key + " " + value);
+                boolean valuee=parentJsonObj.getJSONObject("request_status").getBoolean("accepted");
+                if (!valuee)
+                {
+                    listPartialProfile=new ArrayList<>();
+                    Toast.makeText(UserProfileActivity.this,"load partial profile"+valuee,Toast.LENGTH_SHORT).show();
 
 
-                    Object json = new JSONTokener(value).nextValue();
-                    if (json instanceof JSONObject) {
-                        //  Log.i("jsonvalue", "obj  " + key + "  " + value);
-                        prepareRecylerViewItemData(parentJsonObj.getJSONObject(key), key);
-                    } else if (json instanceof JSONArray) {
-                        // Log.i("jsonvalue", "array " + key + "  " + value);
-                        prepareRecylerViewItemData(parentJsonObj.getJSONArray(key), key);
+                    aboutMeTextView.setText(parentJsonObj.getJSONObject("personal_information").getString("about_yourself"));
+                    hashMap.put(getString(R.string.profession_text),parentJsonObj.getJSONObject("profession").getString("occupation"));
+                    hashMap.put(getString(R.string.present_loaction_text),parentJsonObj.getJSONObject("profile_living_in").getString("country"));
+                    hashMap.put(getString(R.string.home_town),parentJsonObj.getJSONObject("profile_living_in").getString("location"));
+                    hashMap.put(getString(R.string.height_text),parentJsonObj.getJSONObject("personal_information").getString("height_ft")+"' "+parentJsonObj.getJSONObject("personal_information").getString("height_inc")+"''");
+                    hashMap.put(getString(R.string.religion_text),parentJsonObj.getJSONObject("profile_religion").getString("religion"));
+                    hashMap.put(getString(R.string.degree_name_text),parentJsonObj.getJSONObject("education_information").getString("highest_degree"));
 
-                    } else {
-                        // Log.i("jsonvalue", key + "  " + value);
+                    for ( String key : hashMap.keySet() ) {
+
+                        PartialProfileItemModel partialProfileItemModel=new PartialProfileItemModel(key,hashMap.get(key));
+                        listPartialProfile.add(partialProfileItemModel);
                     }
 
+                    PartialProfileViewAdapter partialProfileViewAdapter=new PartialProfileViewAdapter(listPartialProfile);
+                    userProfileInfoRecyclerView.setAdapter(partialProfileViewAdapter);
 
+                }
+                else {
+
+                    Toast.makeText(UserProfileActivity.this,"load full profile"+valuee,Toast.LENGTH_SHORT).show();
+
+                    Iterator iterator = parentJsonObj.keys();
+                    while (iterator.hasNext()) {
+                        String key = (String) iterator.next();
+                        String value = parentJsonObj.getString(key);
+
+
+                        Object json = new JSONTokener(value).nextValue();
+                        if (json instanceof JSONObject) {
+                            prepareRecylerViewItemData(parentJsonObj.getJSONObject(key), key);
+                        } else if (json instanceof JSONArray) {
+                            prepareRecylerViewItemData(parentJsonObj.getJSONArray(key), key);
+
+                        } else {
+                        }
+
+
+                    }
+                    userProfileInfoRecyclerView.setAdapter(new UserProfileExpenadlbeAdapter(getBaseContext(), userProfilesList));
                 }
 
 
@@ -159,7 +152,7 @@ public class UserProfileActivity extends AppCompatActivity {
             }
 
 
-            userProfileInfoRecyclerView.setAdapter(new UserProfileExpenadlbeAdapter(getBaseContext(), userProfilesList));
+
 
 
         }
@@ -173,22 +166,18 @@ public class UserProfileActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... url) {
-
-            //get data from url
-            Response response;
+        Response response;
             SharePref sharePref = new SharePref(UserProfileActivity.this);
             String token = sharePref.get_data("token");
             Request request = null;
 
             request = new Request.Builder()
-                    .url(Constant.BASE_URL + SUB_URL + 316)
+                    .url(Constant.BASE_URL + SUB_URL + id)
                     .addHeader("Authorization", "Token token=" + token)
                     .build();
             try {
                 response = client.newCall(request).execute();
                 String jsonData = response.body().string();
-                /*JSONObject Jobject = new JSONObject(jsonData);
-                return Jobject.toString();*/
                 return jsonData;
 
             } catch (Exception e) {
@@ -206,15 +195,13 @@ public class UserProfileActivity extends AppCompatActivity {
             String childKey = (String) iterator.next();
             String childValue = childJsonObject.getString(childKey);
 
-            //childItemList.add(new UserProfileChild(Constant.profileItemBanglaName(childKey), childValue));
-            childItemList.add(new UserProfileChild(childKey, childValue));
+              childItemList.add(new UserProfileChild(childKey, childValue));
 
 
         }
 
 
-        //userProfilesList.add(new UserProfile(Constant.profileItemBanglaName(key), childItemList));
-        userProfilesList.add(new UserProfile(key, childItemList));
+         userProfilesList.add(new UserProfile(key, childItemList));
 
     }
 
@@ -236,8 +223,7 @@ public class UserProfileActivity extends AppCompatActivity {
         for (int n = 0; n < childJsonArray.length(); n++) {
             JSONObject object = childJsonArray.getJSONObject(n);
 
-            // do some stuff....
-            for (Iterator<String> iter = object.keys(); iter.hasNext(); ) {
+           for (Iterator<String> iter = object.keys(); iter.hasNext(); ) {
                 String childKey = iter.next();
 
                 childNodeValue += object.getString(childKey);
