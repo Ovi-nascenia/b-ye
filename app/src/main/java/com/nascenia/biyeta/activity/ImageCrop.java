33049175f61d16c75d42b236581a66694382b4f6
saiful <saiful@bdipo.com>
2017-12-21 19:18:40 +0600

    Stop image rotation after pick or capture image

--- app/src/main/java/com/nascenia/biyeta/activity/ImageCrop.java
+++ app/src/main/java/com/nascenia/biyeta/activity/ImageCrop.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.DisplayMetrics;
@@ -30,6 +32,7 @@ public class ImageCrop extends Activity {
     private static final float MIN_ZOOM = 1f, MAX_ZOOM = 1f;
     PhotoViewAttacher mAttacher;
 
+    private ExifInterface exifObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +42,34 @@ public class ImageCrop extends Activity {
         setContentView(R.layout.activity_image_crop);
         final ImageView view = (ImageView) findViewById(R.id.imageView1);
         File imgFile = new File(imageUrl);
-        if (imgFile.exists()) {
-            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-            int width = myBitmap.getWidth();
-            int height = myBitmap.getHeight();
+        try {
+            if (imgFile.exists()) {
 
-            DisplayMetrics metrics = new DisplayMetrics();
-            getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            int screenWidth = metrics.widthPixels;
-            if (width >= height) {
-                bitmap = Bitmap.createScaledBitmap(myBitmap, (screenWidth * 10) / 16, ((height * ((screenWidth * 10) / 16)) / width), true);
-            } else {
-                bitmap = Bitmap.createScaledBitmap(myBitmap, ((width * ((screenWidth * 10) / 16)) / height), (screenWidth * 10) / 16, true);
-            }
+                exifObject = new ExifInterface(imageUrl);
+
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                int width = myBitmap.getWidth();
+                int height = myBitmap.getHeight();
+
+                DisplayMetrics metrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(metrics);
+                int screenWidth = metrics.widthPixels;
+                if (width >= height) {
+                    bitmap = Bitmap.createScaledBitmap(myBitmap, (screenWidth * 10) / 16, ((height * ((screenWidth * 10) / 16)) / width), true);
+                } else {
+                    bitmap = Bitmap.createScaledBitmap(myBitmap, ((width * ((screenWidth * 10) / 16)) / height), (screenWidth * 10) / 16, true);
+                }
 
 
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-            byte[] byteArray = byteArrayOutputStream.toByteArray();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                byte[] byteArray = byteArrayOutputStream.toByteArray();
 
-            encoded = Base64.encodeToString(byteArray, Base64.NO_WRAP);
-            Log.i("bitmap", "original: " + encoded);
+                encoded = Base64.encodeToString(byteArray, Base64.NO_WRAP);
+                Log.i("bitmap", "original: " + encoded);
 
-            byte[] decodedString = Base64.decode(encoded, Base64.NO_WRAP);
-            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                byte[] decodedString = Base64.decode(encoded, Base64.NO_WRAP);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
 
            /* if (ImageUpload.beforeProPicUploadValue == 1) {
                 ImageUpload.afterProPicUploadValue = 1;
@@ -84,10 +91,18 @@ public class ImageCrop extends Activity {
 
                 ImageUpload.otherPicBase64 = "data:image/jpeg;base64," + encoded;
             }*/
+                int orientation = exifObject.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                        ExifInterface.ORIENTATION_UNDEFINED);
+                Bitmap imageRotateBitmap = rotateBitmap(decodedByte,orientation);
 
-            view.setImageBitmap(decodedByte);
-            view.setBackgroundColor(Color.rgb(0, 0, 0));
+                //view.setImageBitmap(decodedByte);
+                view.setImageBitmap(imageRotateBitmap);
+                view.setBackgroundColor(Color.rgb(0, 0, 0));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
 
         final Button crop = (Button) findViewById(R.id.crop);
         final Button discart = (Button) findViewById(R.id.discart);
@@ -147,5 +162,45 @@ public class ImageCrop extends Activity {
 
     }
 
-
+    public static Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
+        Matrix matrix = new Matrix();
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_NORMAL:
+                return bitmap;
+            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
+                matrix.setScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                matrix.setRotate(180);
+                break;
+            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
+                matrix.setRotate(180);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_TRANSPOSE:
+                matrix.setRotate(90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
+                break;
+            case ExifInterface.ORIENTATION_TRANSVERSE:
+                matrix.setRotate(-90);
+                matrix.postScale(-1, 1);
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                matrix.setRotate(-90);
+                break;
+            default:
+                return bitmap;
+        }
+        try {
+            Bitmap bmRotated = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            bitmap.recycle();
+            return bmRotated;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
